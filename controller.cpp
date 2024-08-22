114522d365f256decfe4b71f6cce98014925795f
Actat <akio.toyosh@gmail.com>
2024-08-22 22:58:26 +0900

    use start() to set dq_target

--- controller.cpp
+++ controller.cpp
@@ -1,14 +1,21 @@
 #include <cnoid/SimpleController>
 
 class MyController : public cnoid::SimpleController {
+  cnoid::BodyPtr ioBody;
+
 public:
   virtual bool initialize(cnoid::SimpleControllerIO *io) override {
-    cnoid::BodyPtr ioBody = io->body();
+    ioBody = io->body();
     cnoid::Link *joint = ioBody->joint(1);
 
     joint->setActuationMode(cnoid::Link::JointVelocity);
     io->enableIO(joint);
 
+    return true;
+  }
+
+  virtual bool start() override {
+    cnoid::Link *joint = ioBody->joint(1);
     joint->dq_target() = 200;
 
     return true;
