d4e82475b7a8964bddd1c2c338d4fd9c7bebbf23
Actat <akio.toyosh@gmail.com>
2024-08-22 21:35:40 +0900

    format

--- controller.cpp
+++ controller.cpp
@@ -1,21 +1,18 @@
 #include <cnoid/SimpleController>
 
-class MyController : public cnoid::SimpleController
-{
-    public:
-    virtual bool initialize(cnoid::SimpleControllerIO* io) override
-    {
-        cnoid::BodyPtr ioBody = io->body();
-        cnoid::Link* joint = ioBody->joint(1);
+class MyController : public cnoid::SimpleController {
+public:
+  virtual bool initialize(cnoid::SimpleControllerIO *io) override {
+    cnoid::BodyPtr ioBody = io->body();
+    cnoid::Link *joint = ioBody->joint(1);
 
-        joint->setActuationMode(cnoid::Link::JointVelocity);
-        io->enableIO(joint);
+    joint->setActuationMode(cnoid::Link::JointVelocity);
+    io->enableIO(joint);
 
-        joint->dq_target() = 200;
+    joint->dq_target() = 200;
 
-        return true;
-    }
-
+    return true;
+  }
 };
 
 CNOID_IMPLEMENT_SIMPLE_CONTROLLER_FACTORY(MyController)
